8500b7d9c85ce6248f686f4da1ccbad5612ac628
Genaro Soriano <agenaro13@gmail.com>
2020-10-26 21:31:02 +0100

    Amelioration des erreurs d'unité mais il y a encore. \n Avec posibilité de changer des unités.

--- Projet_Cocombustion.py
+++ Projet_Cocombustion.py
@@ -13,80 +13,84 @@ Faudra remplir dans un txt les valeurs :
     - Prix de vente chaque materiau
 """
 
-###########################################################################
-# Cette version contient les corrections de profe mais il a aussi des erreurs
-# des unitées dans la disponibilité maximale
-###########################################################################
-
 from gurobipy import *
 import numpy as np
 
 ###########################################################################
 # Definition des constantes
 ###########################################################################
+# Chaque donnée d'entre vient avec les unites comme des constantes. On peut
+# definir ces unitées ici en bas selon les unites qu'on veut à la fin. Par
+# exemple, si on veut exprimer les résultat en kiloton on met ton = 0.001. 
+# Si on veut rester dans l'unité ton on met ton = 1.
+###########################################################################
+# Unites (Il faut changer que ton selon l'unité qu'on veux avoir)
+ton = 0.001                                     # On va afficher tout en Kton
+kt = 1000 * ton                                 # kton
+euros = 1/1000000                               # Milliard
+###########################################################################
+
 megajoule = 1/3600                              # MWh (facteur pour passer de joules a MWh)
 puissance_nominale = 250                        # MW
 prod_moyenne = 7500                             # Heures
 efficacite = 0.38
 annee_debut = 2016 # Je n'ai pas encorporé celle-ci. Il faudra l'ajouter après
 porcentage_carbon = 0.1                         # Ratio entre Carbon et Biomasse
-instalation_sechage = 0                         # €
-duplic_capacite = 500000                        # €
-capacite_jour = 1500                            # t/jour
-instalation_sechage = 600000                    # €
+instalation_sechage = 0*euros                   # €
+duplic_capacite = 500000*euros                  # €
+capacite_jour = 1500 * ton                      # t/jour
+instalation_sechage = 600000*euros              # €
+capacite_sechage = 150*kt                       # kt/an
 energ_prod = puissance_nominale * prod_moyenne  # MWh
 energ_brut = energ_prod / efficacite            # MWh
-horizon = 20
-
+horizon = 1
 
 dispositif_sechage = True                       # True si on va utliser le dispositive de sechage
 
-# p_achat (euros), dispo (kt)
+# p_achat (euros/ton), humidite, dispo (kt)
 combustibles, p_achat, humid, dispo = multidict({
-        'carb': [100, 0, GRB.INFINITY],
-        'biog': [190, 0, 700],
-        'vert': [25, 0.4, 52],
-        'bois': [0, 0.2, GRB.INFINITY],
-        'recy': [12, 0.05, 85]
+        'carb': [100*euros/ton,   0,      GRB.INFINITY],
+        'biog': [190*euros/ton,   0,      700*kt],
+        'vert': [25*euros/ton,    0.4,    52*kt],
+        'bois': [0*euros/ton,     0.2,    GRB.INFINITY],
+        'recy': [12*euros/ton,    0.05,   85*kt]
     })
 
+# p_achat (euros/t), dispo 1 (kt), dispo 2 (kt), GES (kg), route (km)
 bois_prove, p_achat_bois, dispo_bois_debut, dispo_bois_final, ges_bois, route_bois = multidict({
-'30A':	[128,   18,	21,	0.04,	230],
-'30C':	[120,   21,	43,	0.03,	210],
-'48':	[128,   12,	75,	0.04,	230],
-'07':	[116,   8,	56,	0.03,	200],
-'13':	[44,	47,	51,	0.00,	20],
-'84':	[76,	24,	28,	0.02,	100],
-'83':	[60,	27,	27,	0.01,	60],
-'05':	[100,   15,	21,	0.03,	160],
-'04':	[88,	26,	37,	0.02,	130],
-'200':	[116,   27,	27,	0.03,	200],
-'300':	[156,   56,	56,	0.05,	300],
-'400':	[196,   93,	93,	0.07,	400]
+'30A':	[128*euros/ton,   18*kt,  21*kt,	0.04,	230],
+'30C':	[120*euros/ton,   21*kt,  43*kt,	0.03,	210],
+'48':	[128*euros/ton,   12*kt,  75*kt,	0.04,	230],
+'07':	[116*euros/ton,   8*kt,   56*kt,	0.03,	200],
+'13':	[44*euros/ton,	  47*kt,  51*kt,	0.00,	20],
+'84':	[76*euros/ton,	  24*kt,  28*kt,	0.02,	100],
+'83':	[60*euros/ton,	  27*kt,  27*kt,	0.01,	60],
+'05':	[100*euros/ton,   15*kt,  21*kt,	0.03,	160],
+'04':	[88*euros/ton,	  26*kt,  37*kt,	0.02,	130],
+'200':	[116*euros/ton,   27*kt,  27*kt,	0.03,	200],
+'300':	[156*euros/ton,   56*kt,  56*kt,	0.05,	300],
+'400':	[196*euros/ton,   93*kt,  93*kt,	0.07,	400]
 })
-
-
 
 ###########################################################################
 # Fonctions auxiliaires
 ###########################################################################
+#GJ/t
 def pci_from_humidity(humidity):
     return 18 - 21 * humidity
 
 # PCI en MWh/ton
 def pci(combustible):
     pcigj = 25 if combustible=='carb' else pci_from_humidity(humid[combustible])
-    return pcigj/3.6
+    return pcigj/(3.6 * ton)
 
 # prix unitaire en euros/MWh
 def p_vente(combustible):
-    return 43 if combustible=='carb' else 115
+    return 43*euros if combustible=='carb' else 115*euros
 
 # dispo en kton
 def dispo_bois(prove, annee):
     return dispo_bois_debut[prove] if annee <= 10 else dispo_bois_final[prove]
-
-
 
 ###########################################################################
 # Modelisation 
@@ -98,6 +102,7 @@ model.modelSense = GRB.MAXIMIZE
 ###########################################################################
 # Definition des variables 
 ###########################################################################
+
 # masses en ktons
 MASSE = {}                                               # index combustible, annee
 MASSE_BOIS_PROVE = {}                                    # index provenance, annee
@@ -129,6 +134,7 @@ CONTR_CARB  = []     # 0.9m_c(t) - 0.1 * SUM_1(m_i(t)) >= 0
 CONTR_PROD  = []     # (m_c(t) * PCI_c + SUM_1(m_i(t) * PCI_i)) = Energie Brute = Energie Produit / efficacite
 CONTR_STOCK = []     # m_b + m_v + m_r <= 1500 * 365
 CONTR_BOIS  = []     # m_b = SUM_2(m_bj(t))
+CONTR_SECH  = []     # Capacité maximale de séchage : 150 kt (H8)
 
 for i in range(horizon):
     biomasse = quicksum(MASSE[c,i] for c in combustibles if c != 'carb')
@@ -137,6 +143,7 @@ for i in range(horizon):
     biomasse_brute = quicksum(MASSE[c,i] for c in ['vert', 'recy', 'bois'])
     CONTR_STOCK.append(model.addConstr(biomasse_brute <= 365 * capacite_jour))
     CONTR_BOIS.append(model.addConstr(MASSE['bois', i] ==  quicksum(MASSE_BOIS_PROVE[p,i] for p in bois_prove)))
+    CONTR_SECH.append(model.addConstr(biomasse_brute <= capacite_sechage))
 
 model.write('new.lp')
 model.optimize()
